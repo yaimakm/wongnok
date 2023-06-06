ac9983e90bde7c79be440faca3f71462548ee460
pakawat <99719249+yaimakm@users.noreply.github.com>
2023-06-06 07:40:21 +0700

    initial

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import { config } from 'dotenv';
+import ErrorMiddleware from './middlewares/Error.js';
+import cookieParser from 'cookie-parser';
+
+config({
+  path: './config/config.env',
+});
+const app = express();
+
+//using middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+//importing & using routes
+import product from './routes/productRoutes.js';
+import user from './routes/userRoutes.js';
+import payment from './routes/paymentRoutes.js';
+import other from './routes/otherRoutes.js';
+
+app.use('/api/v1', product);
+app.use('/api/v1', user);
+app.use('/api/v1', payment);
+app.use('/api/v1', other);
+export default app;
+
+app.use(ErrorMiddleware);
